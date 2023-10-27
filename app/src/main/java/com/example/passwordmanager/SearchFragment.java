fa6ad7f27bb1ab8298f1188e702662a0ceeae9e4
Ninezero <kusepz@gmail.com>
2023-10-28 02:00:08 +0900

    비밀번호 관리자

--- app/src/main/java/com/example/passwordmanager/SearchFragment.java
+++ app/src/main/java/com/example/passwordmanager/SearchFragment.java
@@ -10,12 +10,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 
 import com.example.passwordmanager.adapter.SearchAdapter;
 import com.example.passwordmanager.model.Content;
@@ -23,7 +26,13 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.transition.MaterialSharedAxis;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.protobuf.NullValue;
+
 
 public class SearchFragment extends Fragment {
     FirestoreRecyclerOptions<Content> content_options;
@@ -33,6 +42,7 @@ public class SearchFragment extends Fragment {
     RecyclerView recycler_search;
     SearchView searchView;
     FloatingActionButton search_fab_top;
+    LinearLayout search_emptyView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -79,6 +89,7 @@ public class SearchFragment extends Fragment {
             }
         });
 
+        search_emptyView = view.findViewById(R.id.search_view_empty);
         search_fab_top = view.findViewById(R.id.search_fab_top);
         search_fab_top.setOnClickListener(v -> {
             recycler_search.scrollToPosition(0);
@@ -100,9 +111,11 @@ public class SearchFragment extends Fragment {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 if (dy > 0) {
+                    searchView.clearFocus();
                     search_fab_top.show();
                     handler.removeCallbacks(runnable);
                 } else if (dy < 0) {
+                    searchView.clearFocus();
                     search_fab_top.show();
                     handler.removeCallbacks(runnable);
                     if (!recyclerView.canScrollVertically(-1))
@@ -120,15 +133,22 @@ public class SearchFragment extends Fragment {
                     .orderBy("search")
                     .startAt(s.toLowerCase().trim())
                     .endAt(s.toLowerCase().trim() + "\uf8ff");
+            content_query.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    search_emptyView.setVisibility(querySnapshot.isEmpty() ? View.VISIBLE : View.GONE);
+                }
+            });
             content_options = new FirestoreRecyclerOptions.Builder<Content>()
                     .setQuery(content_query, Content.class)
                     .build();
             search_adapter = new SearchAdapter(content_options, getContext());
+            search_adapter.startListening();
             recycler_search.setAdapter(search_adapter);
         } else {
             content_options.getSnapshots().clear();
+            search_emptyView.setVisibility(View.GONE);
         }
-        search_adapter.startListening();
     }
 
     @Override
